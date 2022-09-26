94d9628fdf60aeed97353e0fc367e174e71e6362
David Renshaw <dwrenshaw@gmail.com>
2022-09-26 19:43:14 -0400

    better

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 const MAIN_CSS =
-    `body { font-family: Helvetica, Sans, Arial;
-            font-size: medium;
-             margin-left: auto;
-             margin-right: auto;
-             text-align: center;
-     }
-     div {
-          padding-bottom: 10pt;
-     }
-    .word {
+`body { font-family: Helvetica, Sans, Arial;
+        font-size: medium;
+        margin-left: auto;
+        margin-right: auto;
         text-align: center;
-        font-size: 500%;
-     }
-     .err {
-       font-size: 90%;
-       color: #AA0000;
-     }
-     .title {
-       text-align: center;
-       font-size:500%;
-     }`;
+}
+div {
+  padding-bottom: 10pt;
+}
+.word {
+  text-align: center;
+  font-size: 500%;
+}
+.err {
+  font-size: 90%;
+  color: #AA0000;
+}
+.title {
+  text-align: center;
+  font-size:500%;
+}`;
 
-
-
-export default {
-  async fetch(req, env) {
+async function handle_get(req, env) {
     let url = new URL(req.url);
     console.log("path = ", url.pathname);
     if (url.pathname == "/main.css") {
@@ -32,7 +29,16 @@ export default {
     }
 
     let value = await env.WORDS.get("posit");
-    return (new Response("Hello World. definition of 'posit': " + value));
+  return (new Response("Hello World. definition of 'posit': " + value));
+}
+
+export default {
+  async fetch(req, env) {
+    if (req.method == "GET") {
+      return await handle_get(req, env);
+    } else {
+      return new Response(400);
+    }
   },
 
   async scheduled(event, env, ctx) {
