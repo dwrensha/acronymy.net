0d291c8d172795722ab78fb9fdf252278e32e647
David Renshaw <dwrenshaw@gmail.com>
2022-09-26 20:09:13 -0400

    main page

--- src/index.js
+++ src/index.js
@@ -21,17 +21,35 @@ div {
   font-size:500%;
 }`;
 
+const HEADER =
+`<head><title> acronymy </title><link rel="stylesheet" type="text/css" href="main.css" >
+ </head>`;
+
+const LOOKUP_FORM =
+`<form action="define" method="get">
+ <input name="word" maxlength="100"/><button>find word</button></form>`;
+
 const WORD_OF_THE_DAY_KEY = "word-of-the-day";
 
 async function handle_get(req, env) {
-    let url = new URL(req.url);
-    console.log("path = ", url.pathname);
-    if (url.pathname == "/main.css") {
-      return new Response(MAIN_CSS);
-    }
+  let url = new URL(req.url);
+  console.log("path = ", url.pathname);
+  if (url.pathname == "/main.css") {
+    return new Response(MAIN_CSS);
+  }
 
-    let value = await env.META.get(WORD_OF_THE_DAY_KEY);
-    return (new Response("Hello World. word of the day: " + value));
+  let response_string = "<html>" + HEADER + "<body>";
+  response_string += "<div class=\"title\">Acronymy</div>";
+  response_string += "<div>A user-editable, acronym-only dictionary.</div>";
+  let word_of_the_day = await env.META.get(WORD_OF_THE_DAY_KEY);
+  response_string += "<div>Today's featured word: ";
+  response_string += `<a href="/define?word=${word_of_the_day}">${word_of_the_day}</a>`;
+  response_string += ".</div>"
+  response_string += LOOKUP_FORM;
+  response_string += "</body></html>";
+
+  return new Response(response_string,
+                      {headers: {'content-type': 'text/html;charset=UTF-8'}});
 }
 
 export default {
