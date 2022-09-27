b38bee3c491034db7fde6cfb0e25797cbc92fe9f
David Renshaw <dwrenshaw@gmail.com>
2022-09-26 21:00:12 -0400

    definition form

--- src/index.js
+++ src/index.js
@@ -29,6 +29,15 @@ const LOOKUP_FORM =
 `<form action="define" method="get">
  <input name="word" maxlength="100"/><button>find word</button></form>`;
 
+function define_form(word) {
+  return `<form action=\"define\" method=\"get\">
+          <input name=\"word\" value=\"${word}\" type=\"hidden\"/>
+          <input name=\"definition\" maxlength=\"2000\"/>
+          <button>submit definition</button></form>`;
+}
+
+const HOME_LINK = "<a href=\"/\">home</a>";
+
 const WORD_OF_THE_DAY_KEY = "word-of-the-day";
 const WORD_LIST_KEY = "word-list";
 
@@ -44,6 +53,8 @@ async function get_word_list(env) {
   }
   return WORD_LIST;
 }
+
+
 
 async function handle_get(req, env) {
   let url = new URL(req.url);
@@ -64,6 +75,26 @@ async function handle_get(req, env) {
     if (!word_list.has(word)) {
       return new Response(word + " is not in the word list", { status: 400 })
     }
+
+    if (url.searchParams.get('definition')) {
+
+    } else {
+      response_string += `<div class=\"word\">${word}</div>`;
+      console.log(`looking up |${word}|`);
+      let definition = await env.WORDS.get(word);
+      if (definition) {
+        let def_words = definition.split(" ");
+        response_string += "<div>"
+        for (let def_word of def_words) {
+          response_string += ` <a href="/define?word=${def_word}">${def_word}</a> `;
+        }
+        response_string += "</div>";
+      } else {
+        response_string += "<div>this word has no definition yet</div>";
+      }
+      response_string += define_form(word);
+      response_string += HOME_LINK;
+    }
   } else {
     response_string += "<div class=\"title\">Acronymy</div>";
     response_string += "<div>A user-editable, acronym-only dictionary.</div>";
@@ -72,9 +103,9 @@ async function handle_get(req, env) {
     response_string += `<a href="/define?word=${word_of_the_day}">${word_of_the_day}</a>`;
     response_string += ".</div>"
     response_string += LOOKUP_FORM;
-    response_string += "</body></html>";
   }
 
+  response_string += "</body></html>";
   return new Response(response_string,
                       {headers: {'content-type': 'text/html;charset=UTF-8'}});
 }
