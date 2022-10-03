c5de4dbacde93dfb7cce0567a1572fac8b314368
David Renshaw <dwrenshaw@gmail.com>
2022-10-03 12:25:41 -0400

    implement login

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const MAIN_CSS =
         margin-right: auto;
         text-align: center;
 }
+.definition {
+   font-size: 28px;
+}
 div {
   padding-bottom: 10pt;
 }
@@ -34,12 +37,23 @@ div {
 }
 input[name="definition"] {
    width: 100%;
-   font-size: 20px;
+   font-size: 22px;
 }
 input[name="word"] {
    font-size: 16px;
 }
-
+.footer {
+  width: 500px;
+  margin: auto;
+  font-size: 14px;
+}
+.footer a {
+  float: left;
+}
+.footer form {
+  float: right;
+  font-style: italic;
+}
 `;
 
 const HEADER =
@@ -61,6 +75,24 @@ function define_form(word) {
 }
 
 const HOME_LINK = "<a href=\"/\">home</a>";
+
+function render_def_footer(word, maybe_username) {
+  let result = `<div class="footer">
+                    <hr>
+                    <a href=\"/\">Acronymy</a>`
+  if (maybe_username) {
+    result += `<form action="logout">logged in as ${maybe_username}
+               <input name=\"word\" value=\"${word}\" type=\"hidden\"/>
+               <button>logout</button></form>`
+  } else {
+      result += `<form action="login"><input name="username" placeholder="username" size="10"/>
+                  <input name=\"word\" value=\"${word}\" type=\"hidden\"/>
+                    <button>log in</button>`;
+  }
+  result += `</form></div>`;
+  return result;
+}
+
 
 const WORD_OF_THE_DAY_KEY = "word-of-the-day";
 const WORD_LIST_KEY = "word-list";
@@ -127,7 +159,7 @@ function render_definition(definition, metadata) {
   let response_string = "";
   if (definition) {
     let def_words = definition.split(" ");
-    response_string += "<div>"
+    response_string += `<div class="definition">`
     for (let def_word of def_words) {
       response_string += ` <a href="/define?word=${def_word}">${def_word}</a> `;
     }
@@ -136,6 +168,9 @@ function render_definition(definition, metadata) {
       let time = new Date(metadata.time);
       response_string += `<div class="attribution">`;
       response_string += `—defined ${time.toUTCString()}`;
+      if (metadata.user) {
+        response_string += ` by ${metadata.user}`;
+      }
       response_string += `</div>`;
     }
   } else {
@@ -144,10 +179,33 @@ function render_definition(definition, metadata) {
   return response_string;
 }
 
+// returns either `{valid: true}` or
+// {invalid: true, reason: <string> }.
+function validate_username(username) {
+  if (!username.match(/^[0-9a-zA-Z]+$/)) {
+    return {invalid: true, reason: "username must be alphanumeric"};
+  }
+  if (username.length > 20) {
+    return {invalid: true, reason: "username must be at most 20 characters"};
+  }
+  return {valid: true};
+}
+
 async function handle_get(req, env) {
   let url = new URL(req.url);
   if (url.pathname == "/main.css") {
     return new Response(MAIN_CSS);
+  }
+
+  let username = null;
+  if (req.headers.has('Cookie')) {
+    for (let cookie of req.headers.get('Cookie').split(";")) {
+      let components = cookie.split('=');
+      let name = components[0].trim();
+      if (name == 'username') {
+        username = components[1];
+      }
+    }
   }
 
   let response_string = "<html>" + HEADER + "<body>";
@@ -182,11 +240,15 @@ async function handle_get(req, env) {
               if (req.headers.has('cf-connecting-ip')) {
                 metadata['ip'] = req.headers.get('cf-connecting-ip');
               }
+              if (username && validate_username(username).valid) {
+                metadata['user'] = username;
+              }
               let p1 = env.WORDS.put(word, new_def, {metadata});
-              let p2 = env.WORDS_LOG.put(word + ":" + now, new_def, metadata);
+              let p2 = env.WORDS_LOG.put(word + ":" + now, new_def, {metadata});
               await Promise.all([p1, p2]);
               definition = new_def;
             } catch (e) {
+              console.log(e);
               error_message =
                 "<p>error (daily quota?) while attempting to write definition</p>"
               error_message +=
@@ -205,8 +267,39 @@ async function handle_get(req, env) {
       if (error_message) {
         response_string += `<div class="err"> ${error_message} </div>`;
       }
-      response_string += HOME_LINK;
+      response_string += render_def_footer(word, username);
     }
+  } else if (url.pathname == "/login") {
+    let username = url.searchParams.get('username');
+    if (!username) {
+      return new Response("need to specify username", { status: 400 })
+    }
+    let username_validation = validate_username(username);
+    if (!username_validation.valid) {
+      response_string += `<div class="err"> ${username_validation.reason} </div>`;
+      response_string += HOME_LINK;
+    } else {
+      let word = url.searchParams.get('word');
+      let location = "/";
+      if (word) {
+        location = `/define?word=${word}`;
+      }
+      return new Response("",
+                          {status: 302,
+                           headers: {'Location': location,
+                                     'Set-Cookie': `username=${username}`}});
+    }
+  } else if (url.pathname == "/logout") {
+    let word = url.searchParams.get('word');
+    let location = "/";
+    if (word) {
+      location = `/define?word=${word}`;
+    }
+    return new Response("",
+                        {status: 302,
+                         headers: {'Location': location,
+                                   'Set-Cookie':
+                                      `username=X; expires=Thu, 01 Jan 1970 00:00:00 GMT`}});
   } else {
     response_string += "<div class=\"title\">Acronymy</div>";
     response_string += "<div>A user-editable, acronym-only dictionary.</div>";
