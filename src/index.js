6250424236b2e0a9318cdc1150502850bc3d1cc3
David Renshaw <dwrenshaw@gmail.com>
2022-10-17 21:57:45 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -117,13 +117,12 @@ a[class="home-link"] {
 const HEADER =
 `<head>
  <meta name="viewport" content="width=device-width">
-<title> acronymy </title><link rel="stylesheet" type="text/css" href="main.css" >
+<title> acronymy </title><link rel="stylesheet" type="text/css" href="/main.css" >
  </head>`;
 
 function define_form(word) {
-  return `<div class="definition-form full-width" >
-          <form action=\"define\" method=\"get\">
-          <input name=\"word\" value=\"${word}\" type=\"hidden\"/>
+  return `<div class="definition-form full-width">
+          <form action="/define/${word}" method="post">
           <input name=\"definition\" maxlength=\"2000\" placeholder="enter new definition" class="definition-input-text" autofocus required/>
           <br>
           <button>submit</button></form>
@@ -134,7 +133,7 @@ function render_home_footer(maybe_username) {
   let result = `<div class="footer full-width">
                     <hr>
                 <div class="footer-row">`;
-  result += `<form action="define" method="get">
+  result += `<form action="/define" method="get">
              <input name="word" maxlength="100" size="15"
                     placeholder="enter word" autofocus required/>
              <button>look up</button></form>`;
@@ -142,11 +141,11 @@ function render_home_footer(maybe_username) {
                 href="https://github.com/dwrensha/acronymy-workers">source code</a>`
 
   if (maybe_username) {
-    result += `<form class="logged-in" action="logout">logged in as ${maybe_username}
+    result += `<form class="logged-in" action="/logout">logged in as ${maybe_username}
                <button>log out</button></form>`
   } else {
     result +=
-      `<form action="login"><input name="username" placeholder="username" size="10" required/>
+      `<form action="/login"><input name="username" placeholder="username" size="10" required/>
        <button>log in</button>`;
   }
   result += `</form></div></div>`;
@@ -158,7 +157,7 @@ function render_def_footer(word, maybe_username) {
                 <hr>
                 <div class="footer-row">`;
 
-  result += `<form action="define" method="get">
+  result += `<form action="/define" method="get">
              <input name="word" maxlength="100" size="15"
                     placeholder="enter word" required/>
              <button>look up</button></form>`;
@@ -166,12 +165,12 @@ function render_def_footer(word, maybe_username) {
   result += `<a class="home-link" href=\"/\">Acronymy</a>`
 
   if (maybe_username) {
-    result += `<form action="logout">logged in as ${maybe_username}
+    result += `<form action="/logout">logged in as ${maybe_username}
                <input name=\"word\" value=\"${word}\" type=\"hidden\"/>
                <button>log out</button></form>`
   } else {
     result +=
-      `<form action="login"><input name="username" placeholder="username" size="10" required/>
+      `<form action="/login"><input name="username" placeholder="username" size="10" required/>
        <input name=\"word\" value=\"${word}\" type=\"hidden\"/>
        <button>log in</button>`;
   }
@@ -253,7 +252,7 @@ function render_definition(word, definition, metadata) {
     let def_words = definition.split(" ");
     response_string += `<div class="definition">`
     for (let def_word of def_words) {
-      response_string += ` <a href="/define?word=${def_word}">${def_word}</a> `;
+      response_string += ` <a href="/define/${def_word}">${def_word}</a> `;
     }
     response_string += "</div>";
     if (metadata && metadata.time) {
@@ -313,11 +312,18 @@ async function handle_get(req, env) {
   let response_string = "<html>" + HEADER + "<body>";
 
   if (url.pathname == "/define") {
+    // Result of submitting the "look up" form.
+    // Redirect to "/define/<word>".
     let word = url.searchParams.get('word');
     if (!word) {
       return new Response("need to specify word", { status: 400 })
     }
     word = word.toLowerCase().trim();
+    return new Response("",
+                        {status: 302,
+                         headers: {'Location': `/define/${word}`, }});
+  } else if (url.pathname.startsWith("/define/")) {
+    let word = url.pathname.slice("/define/".length);
     let { value, metadata } = await env.WORDS.getWithMetadata(word);
     let definition = value;
     if (!definition && !(await WORD_LIST.is_word(word, env))) {
@@ -326,7 +332,15 @@ async function handle_get(req, env) {
     } else {
       response_string += `<div class=\"word\"><b>${word}</b></div>`;
       let error_message = null;
-      let proposed_definition = url.searchParams.get('definition');
+      let proposed_definition = null;
+      if (req.method == "POST") {
+        const form_data = await req.formData();
+        for (const entry of form_data.entries()) {
+          if (entry[0] == "definition") {
+            proposed_definition = entry[1];
+          }
+        }
+      }
       if (proposed_definition) {
         let def_words = proposed_definition.trim().toLowerCase().split(/[\s+]/);
         let validation_result = await validate_definition(def_words, word, env);
@@ -347,7 +361,9 @@ async function handle_get(req, env) {
               let p1 = env.WORDS.put(word, new_def, {metadata});
               let p2 = env.WORDS_LOG.put(word + ":" + now, new_def, {metadata});
               await Promise.all([p1, p2]);
-              definition = new_def;
+              return new Response("",
+                                  {status: 303,
+                                   headers: {'Location': `/define/${word}` }});
             } catch (e) {
               console.log(e);
               error_message =
@@ -374,7 +390,7 @@ async function handle_get(req, env) {
     let word = url.searchParams.get('word');
     let location = "/";
     if (word) {
-      location = `/define?word=${word}`;
+      location = `/define/${word}`;
     }
 
     let username = url.searchParams.get('username') || "";
@@ -392,7 +408,7 @@ async function handle_get(req, env) {
     let word = url.searchParams.get('word');
     let location = "/";
     if (word) {
-      location = `/define?word=${word}`;
+      location = `/define/${word}`;
     }
     return new Response("",
                         {status: 302,
@@ -424,7 +440,7 @@ async function handle_get(req, env) {
     entries.reverse();
 
     response_string += `<div>history of definitions for
-                        <a href="/define?word=${word}">${word}</a>:</div>`;
+                        <a href="/define/${word}">${word}</a>:</div>`;
     response_string += `<ul class="history full-width">`
     let promises = [];
     for (let entry of entries) {
@@ -463,14 +479,14 @@ async function handle_get(req, env) {
     response_string += "<li>Recently defined words include: ";
     for (let ii = 0; ii < status.recently_defined.length; ++ii) {
       let w = status.recently_defined[ii];
-      response_string += `<a href="/define?word=${w}">${w}</a>`;
+      response_string += `<a href="/define/${w}">${w}</a>`;
       if (ii+1 < status.recently_defined.length) {
         response_string += ", ";
       }
     }
     response_string += ".</li>";
     response_string += "<li>Today's featured word is ";
-    response_string += `<b><a href="/define?word=${word_of_the_day}">${word_of_the_day}</a></b>.`;
+    response_string += `<b><a href="/define/${word_of_the_day}">${word_of_the_day}</a></b>.`;
     response_string += "</li>";
     response_string += `</div>`;
     response_string += render_home_footer(username);
@@ -483,7 +499,7 @@ async function handle_get(req, env) {
 
 export default {
   async fetch(req, env) {
-    if (req.method == "GET") {
+    if (req.method == "GET" || req.method == "POST") {
       return await handle_get(req, env);
     } else {
       return new Response("bad request",
