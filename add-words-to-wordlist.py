7b5a7b8cbae56793e89dfce1e95ca28ff3b5eb74
David Renshaw <dwrenshaw@gmail.com>
2022-12-26 20:22:53 -0500

    add-words-to-wordlist.py

---
+++ add-words-to-wordlist.py
@@ -0,0 +1,109 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description="add some words to the wordlist")
+parser.add_argument("--new_words_file", type=str, required=True, help="file containing new words")
+parser.add_argument("--preview", type=bool, default=False)
+args = parser.parse_args()
+
+preview_arg = "--preview=false"
+if args.preview:
+    preview_arg = "--preview=true"
+
+words_by_initial = {}
+all_words = set()
+
+completed_process = subprocess.run(
+      ["wrangler",
+       "kv:key",
+       "get",
+       "--binding=META",
+       preview_arg,
+       "word-list"],
+      stdout=subprocess.PIPE)
+
+if completed_process.returncode != 0:
+    print("download failed")
+    exit(1)
+
+for word in completed_process.stdout.decode("utf-8").split("\n"):
+    if len(word) == 0: # can happen on last line
+        continue
+    all_words.add(word)
+    initial = word[0]
+    if not initial in words_by_initial:
+        words_by_initial[initial] = []
+    words_by_initial[initial].append(word)
+
+
+new_words = set()
+words_file = open(args.new_words_file, "r")
+
+for line in words_file.readlines():
+    word = line.strip()
+    if len(word) == 0:
+        continue
+    if word in all_words:
+        continue
+
+    new_words.add(word)
+    all_words.add(word)
+    initial = word[0]
+    if not initial in words_by_initial:
+        words_by_initial[initial] = []
+    words_by_initial[initial].append(word)
+
+# just double check...
+total_wbi = 0;
+for k in words_by_initial:
+    total_wbi += len(words_by_initial[k])
+
+assert(total_wbi == len(all_words))
+
+new_words_file = open("/tmp/new-words.txt", "w")
+for new_word in sorted(new_words):
+    new_words_file.write(new_word + "\n")
+
+
+open("/tmp/newly-added-words.txt", "w")
+for new_word in sorted(new_words):
+    new_words_file.write(new_word + "\n")
+
+print("{} newly added words".format(len(new_words)))
+
+if len(new_words) == 0:
+    print("no work to do")
+    exit(0)
+
+NEW_WORDLIST_FILE = "/tmp/new-wordlist.txt"
+with open(NEW_WORDLIST_FILE, "w") as f:
+    for word in sorted(all_words):
+        f.write(word + "\n")
+
+completed_process = subprocess.run(
+    ["wrangler",
+     "kv:key",
+     "put",
+     "--binding=META",
+     preview_arg,
+     "word-list",
+     "--path={}".format(NEW_WORDLIST_FILE)],
+    check=True)
+
+for (k,v) in words_by_initial.items():
+    v1 = "\n".join(v)
+    filename = "/tmp/wordlist-{}.txt".format(k)
+    with open(filename, 'w') as f:
+        f.write(v1)
+    print("word-list-" + k)
+    subprocess.run(
+        ["wrangler",
+         "kv:key",
+         "put",
+         "--binding=META",
+         preview_arg,
+         "word-list-" + k,
+         "--path={}".format(filename)],
+        check=True)
+
+
