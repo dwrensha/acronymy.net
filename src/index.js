adf30317717a1406c6a4725bd901379955aafb55
David Renshaw <dwrenshaw@gmail.com>
2023-06-07 10:29:24 -0400

    update robots.txt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { MAIN_CSS } from "./css.js";
 
 const ROBOTS_TXT =
-`User agent: serpstatbot
-Disallow: /
-
-User-agent: *
-Disallow: /history`
+`User-agent: *
+Disallow: /history`;
 
 const FAVICON =`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 135.47 135.47">
  <rect width="100%" height="100%" fill="white"/>
