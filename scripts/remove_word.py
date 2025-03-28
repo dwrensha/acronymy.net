96407e2c8aba5c3ef91af0c28b1ffa7e4a7b5955
David Renshaw <dwrenshaw@gmail.com>
2025-03-28 15:48:40 -0400

    update remove_word.py and remove /refresh-status path

--- scripts/remove_word.py
+++ scripts/remove_word.py
@@ -25,12 +25,15 @@ def run_sqlite_query(query):
                             text=True, check=True)
     return json.loads(result.stdout)[0]
 
+print("checking whether word exists...")
 query0 = "select rowid from words where word = '{word}'".format(word=WORD)
 query0_results = run_sqlite_query(query0)
 if len(query0_results['results']) == 0:
     print("Word is not in list. Aborting.")
     sys.exit(1)
 
+
+print("checking whether any definition uses the word...")
 query1 = "select * from defs_log where def like '% {word} %' or def like '{word} %' or def like '% {word}'".format(word=WORD)
 
 query1_results = run_sqlite_query(query1)
@@ -38,6 +41,7 @@ if len(query1_results['results']) > 0:
     print("Word is already in use. Aborting.")
     sys.exit(1)
 
+print("checking whether the word is defined...")
 query2 = "select * from defs where word = '{word}'".format(word=WORD)
 query2_results = run_sqlite_query(query2)
 if len(query2_results['results']) > 0:
@@ -60,20 +64,30 @@ if len(query2_results['results']) > 0:
     if ENV == "dev":
         preview = "--preview true"
 
-    command = """npx wrangler -e {env} kv key delete {word} --binding WORDS {preview}""".format(env=ENV, word=WORD, preview=preview)
+    command = """npx wrangler -e {env} kv key delete {word} --binding WORDS {preview} --remote""".format(env=ENV, word=WORD, preview=preview)
     subprocess.run(command, shell=True, text=True, check=True)
 
+print("deleting the word...")
 query3 = "delete from words where word = '{word}'".format(word=WORD)
 query3_results = run_sqlite_query(query3)
 if not query3_results["success"]:
     print("Failed to delete word. Aborting.")
     sys.exit(1)
 
+print("updating status.total_num_words...")
 query4 = "UPDATE status SET total_num_words = total_num_words - 1";
 query4_results = run_sqlite_query(query4)
 if not query4_results["success"]:
     print("Failed to update word count. Aborting.")
     sys.exit(1)
 
+
+print("clearing cached homepage status...")
+preview = "--preview false"
+if ENV == "dev":
+    preview = "--preview true"
+
+command = """npx wrangler -e {env} kv key delete status --binding META {preview} --remote""".format(env=ENV, word=WORD, preview=preview)
+subprocess.run(command, shell=True, text=True, check=True)
+
 print("success!")
-print("To update the cached status on the homepage, visit /refresh-status as admin.")
